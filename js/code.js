f6668a2fbe562a5829656d302b49d29619994b84
Michał Kuwik <Michal7074@poczta.fm>
2019-04-18 21:16:01 +0200

    get DOM elements in code.js

---
+++ js/code.js
@@ -0,0 +1,5 @@
+//DOM
+const searchFormEl = document.querySelector('#search-form');
+const searchInputEl = document.querySelector('#search-input');
+const searchOptionEl = document.querySelector('#search-option');
+const searchResultEl = document.querySelector('#search-result')
