0b2df4a010f9ad5a037bbe01971a989ce478b126
Michał Kuwik <Michal7074@poczta.fm>
2019-04-18 21:26:12 +0200

    add variable with base-url and create function with eventlistener about choose option

--- js/code.js
+++ js/code.js
@@ -3,3 +3,12 @@ const searchFormEl = document.querySelector('#search-form');
 const searchInputEl = document.querySelector('#search-input');
 const searchOptionEl = document.querySelector('#search-option');
 const searchResultEl = document.querySelector('#search-result')
+
+const apiBaseURL = 'https://swapi.co/api';
+let serachOption = 'films';
+
+//change option function
+searchOptionEl.addEventListener('change', function() {
+    searchOption = this.value;
+    console.log(searchOption);
+});
